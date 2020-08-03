26215ffb8f3af7ef89d25cb267d2b422916336e7
co3oing <co3oing@naver.com>
2020-08-03 14:20:03 +0900

    add button

--- src/App.js
+++ src/App.js
@@ -1,50 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food({ name, picture, rating}) {
-    return (
-        <div>
-         <h2>I like { name }</h2>
-         <h4>{rating}/5.0</h4>
-         <img src={picture} alt={name}/>
-        </div>
-    );
-}
-
-const foodILike = [
-    {   id: 1,
-        name: 'Kimchi',
-        image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
-        rating: 5,
-    },
-    {
-        id: 2,
-        name: 'Samgyeopsal',
-        image:
-  'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
-        rating: 4.9,
-    },
-    {
-        id: 3,
-        name: 'Bibimbap',
-        image:
-  'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb',
-        rating: 3,
+class App extends React.Component {
+    state = {
+        count: 0,
+    };
+    render(){
+        return(
+            <div>
+                <h1>The number is: {this.state.count}</h1>
+                <button>Add</button>
+                <button>Minus</button>
+            </div>
+        );
     }
-];
-
-function App() {
-    return (
-        <div>
-            {foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>))}
-        </div>
-    );
 }
-
-Food.propTypes = {
-    name: PropTypes.string.isRequired,
-    picture: PropTypes.string.isRequired,
-    rating: PropTypes.number,
-};
 
 export default App;
