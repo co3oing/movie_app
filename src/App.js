260ef976e94790a73c4e906b61a85b9340ab7a2e
co3oing <co3oing@naver.com>
2020-08-03 13:35:10 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+function Food(props) {
+    console.log(props);
+    return <h1>I like { props.fav }</h1>;
+}
+
 function App() {
-  return <div className="App" />;
+    return (
+        <div>
+            <h1>Hello</h1>
+            <Food fav="kimchi" />
+            <Food fav="ramen" />
+
+        </div>
+    );
 }
 
 export default App;
