ca6c4b9c9fd4ee35924a43881aa6aeddb60a093e
co3oing <co3oing@naver.com>
2020-08-03 14:00:28 +0900

    complete 04-3

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ function Food({ name, picture }) {
     return (
         <div>
          <h2>I like { name }</h2>
-         <img src={picture} />
+         <img src={picture} alt={name}/>
         </div>
     );
 }
 
 const foodILike = [
-    {
+    {   id: 1,
         name: 'Kimchi',
         image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
     },
     {
+        id: 2,
         name: 'Samgyeopsal',
         image:
   'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
     },
     {
+        id: 3,
         name: 'Bibimbap',
         image:
   'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb',
@@ -29,7 +31,7 @@ const foodILike = [
 function App() {
     return (
         <div>
-            {foodILike.map(dish => (<Food name={dish.name} picture={dish.image} />))}
+            {foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} />))}
         </div>
     );
 }
