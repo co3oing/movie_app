f1d0d1dd9861748f4f0f41bda9b0b3f7b0bb3c11
co3oing <co3oing@naver.com>
2020-08-04 10:53:12 +0900

    { movies: movies } = { movies }

--- src/App.js
+++ src/App.js
@@ -8,8 +8,13 @@ class App extends React.Component {
     };
 
     getMovies = /* getMovies()는 시간이 필요함 */ async () => {
-        const movies = /* axios.get()의 실행을 기다림 (async 안에서만 작동) */ await axios.get('https://yts-proxy.now.sh/list_movies.json');
-        console.log(movies.data.data.movies);
+        const {
+            data: { // data->
+                data: { movies }, // data->movies
+            },
+        } = /* axios.get()의 실행을 기다림 (async 안에서만 작동) */ await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        this.setState({ movies });
+        // = this.setState({ /* state */ movies: /* data */ movies }); 의 축약 형태의 코드
     }
 
     componentDidMount() {
