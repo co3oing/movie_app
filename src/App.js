db59956d74db6780fc90391fe778a7c1e4705b20
co3oing <co3oing@naver.com>
2020-08-04 10:46:42 +0900

    movies.data.data.movies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
     state = {
         isLoading: true,
+        movies: [],
     };
 
+    getMovies = /* getMovies()는 시간이 필요함 */ async () => {
+        const movies = /* axios.get()의 실행을 기다림 (async 안에서만 작동) */ await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        console.log(movies.data.data.movies);
+    }
+
     componentDidMount() {
-        setTimeout(() => {
-            this.setState({ isLoading: false });
-        }, 6000);
+        // 영화 데이터 로딩
+        this.getMovies();
     }
 
     render(){
