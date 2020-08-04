be551deb0ce29bc5d111ebe44ddf6f6cb880e7fc
co3oing <co3oing@naver.com>
2020-08-04 13:21:46 +0900

    add css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
     state = {
@@ -41,9 +42,9 @@ class App extends React.Component {
                                     summary = { movie.summary }
                                     poster = { movie.medium_cover_image }
                                 /> 
-                       ))} 
+                               ))} 
                                 </div>
-                                )}
+                                      )}
                                 </section>
                              );
                                 }
