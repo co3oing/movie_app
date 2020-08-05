3aa0db30265bfbb0e0856e7779c160f492f754cc
co3oing <co3oing@naver.com>
2020-08-05 13:37:23 +0900

    Updates

---
+++ precache-manifest.d7feb2122b75b2afd78ee46ebe143dea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ebb79740c4f1df7d44b8315c94c819c",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "8fc3e207749fd99be926",
+    "url": "/movie_app/static/css/main.f3ddcedb.chunk.css"
+  },
+  {
+    "revision": "a0054564cd7082ef0d3c",
+    "url": "/movie_app/static/js/2.59df1732.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app/static/js/2.59df1732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fc3e207749fd99be926",
+    "url": "/movie_app/static/js/main.ad3afeaf.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
