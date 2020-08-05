4257ec5454c4ba07e6bf5eef8a6d944766d26665
co3oing <co3oing@naver.com>
2020-08-05 12:56:38 +0900

    Navigation.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from './routes/Home';
 function App() {
     return (
         <HashRouter>
-            <Route path="/" exact={true} component={Home} />
+            <Route path="/" /* path props가 정확히 /인 경우에만 Home 컴포넌트 출력 */ exact={true} component={Home} />
             <Route path="/about" component={About} />
         </HashRouter>
     );
