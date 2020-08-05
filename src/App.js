032ec8e1c5b79559b89864fc2c402d29f8fac0fd
co3oing <co3oing@naver.com>
2020-08-05 13:24:26 +0900

    08-4

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { HashRouter, Route } from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
 import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
 function App() {
     /* Link, Router 컴포넌트는 반드시 HashRouter 안에 포함되어야 한다. */
@@ -12,6 +13,7 @@ function App() {
             <Navigation /> 
             <Route path="/" /* path props가 정확히 /인 경우에만 Home 컴포넌트 출력 */ exact={true} component={Home} />
             <Route path="/about" component={About} />
+            <Route path="/movie-detail" component={Detail} />
         </HashRouter>
     );
 }
